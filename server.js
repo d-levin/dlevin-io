0b10410983b6a941144a17ee58a9f418cd76378b
d-levin <dennis.a.levin@gmail.com>
2016-02-26 16:42:01 -0800

    Port change

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ server.get('/', function(req, res) {
   res.send('Hello world!');
 });
 
-server.listen(PORT, function() {
-  console.log('Server is listening on port ' + PORT)
+server.listen(process.env.PORT || PORT, function() {
+  console.log('Server is listening...')
 });
