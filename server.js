59971a065cabd9d8fd0b9583a96dc147bd9ed3db
d-levin <dennis.a.levin@gmail.com>
2016-02-28 22:50:03 -0800

    Commit

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-var express = require('express');
+// Dependencies
+const express = require('express');
+const mysql = require('mysql');
+
+// Global variables
 var server = express();
 
-const PORT = 8080;
+// Connection to local MySQL db
+var db = mysql.createConnection({
+  host:     '127.0.0.1',
+  user:     'root',
+  password: 'root',
+  database: 'gear_closet'
+});
+
+// For local testing
+const TESTPORT = 8080;
 
 server.get('/', function(req, res) {
   res.send('Hello world!');
 });
 
-server.listen(process.env.PORT || PORT, function() {
+server.listen(process.env.PORT || TESTPORT, function() {
   console.log('Server is listening...')
 });
